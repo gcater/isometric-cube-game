928499cc8a0a2cd42e4780dc1ecba591cfbf7f62
Geoffrey Cater <geoffcater123@gmail.com>
2026-05-19 17:20:21 -0400

    added eye enemy

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 @onready var game_floor = $Floor
 @onready var player = $Player
+@onready var eye_enemy = $EyeEnemy
 
 func _ready():
 
 	player.game_floor = game_floor
-
+	eye_enemy.game_floor = game_floor
 	player.grid_pos = Vector2i(3, 3)
-
+	eye_enemy.grid_pos = Vector2i(5, 5)
 	player.snap_to_grid()
+	eye_enemy.snap_to_grid()
 
-	print("WORLD SET PLAYER TO: ", player.grid_pos)
+	# print("WORLD SET PLAYER TO: ", player.grid_pos)
