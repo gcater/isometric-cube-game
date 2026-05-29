53a9d751f81f8808e882658267619398ade82c45
Geoffrey Cater <geoffcater123@gmail.com>
2026-05-29 16:39:24 -0400

    bullet is going direction of player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,12 +7,25 @@ extends Node2D
 @export var move_delay := 0.15
 
 
+# const DIRS_8 := [
+# 	Vector2i(0, -1),
+# 	Vector2i(1, -1),
+# 	Vector2i(1, 0),
+# 	Vector2i(1, 1),
+# 	Vector2i(0, 1),
+# 	Vector2i(-1, 1),
+# 	Vector2i(-1, 0),
+# 	Vector2i(-1, -1),
+# ]
 
 var game_floor
 var grid_pos := Vector2i.ZERO
 var move_timer := 0.0
 var target_position := Vector2.ZERO
-var facing_dir := Vector2i(0, 1)
+var facing_dir := Vector2i(1, 0) # Default facing right
+var attack_index := 2
+#var attack_dir : Vector2i  = DIRS_8[attack_index]
+
 
 func _ready():
 	add_to_group("player")
@@ -23,39 +36,44 @@ func _ready():
 func setup(floor_ref, start_pos):
 	game_floor = floor_ref
 	grid_pos = start_pos
+	sprite.play("right")
 
 	game_floor.add_entity(self, grid_pos)
-	target_position = game_floor.position + game_floor.grid_to_iso(grid_pos)
-	position = target_position
+	snap_to_grid()	
 	
 
 func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		shoot_attack_tile()
 	position = position.lerp(target_position, move_speed * delta)
 	move_timer -= delta
 	if move_timer > 0:
 		return 
 	if Input.is_action_pressed("left"):
+		facing_dir = Vector2i(-1, 0)
 		sprite.play("left")
-		try_move(Vector2i(-1, 0))
+		try_move(facing_dir)
 		move_timer = move_delay
 
 	elif Input.is_action_pressed("right"):
+		facing_dir = Vector2i(1, 0)
 		sprite.play("right")
-		try_move(Vector2i(1, 0))
+		try_move(facing_dir)
 		move_timer = move_delay
 
 	elif Input.is_action_pressed("up"):
+		facing_dir = Vector2i(0, -1)
 		sprite.play("up")
-		try_move(Vector2i(0, -1))
+		try_move(facing_dir)
 		move_timer = move_delay
 
 	elif Input.is_action_pressed("down"):
+		facing_dir = Vector2i(0, 1)	
 		sprite.play("down")
-		try_move(Vector2i(0, 1))
+		try_move(facing_dir)
 		move_timer = move_delay
 
-	if Input.is_action_just_pressed("attack"):
-		shoot_attack_tile()
+	
 
 func try_move(dir: Vector2i):
 
@@ -81,3 +99,11 @@ func shoot_attack_tile():
 	var start_pos := grid_pos - facing_dir
 
 	attack.setup(game_floor, start_pos, facing_dir)
+
+# func update_attack_tile():
+# 	attack_dir = DIRS_8[attack_index]
+
+# 	var attack_pos = grid_pos + attack_dir
+
+# 	attack_tile.grid_pos = attack_pos
+# 	attack_tile.snap_to_grid()
